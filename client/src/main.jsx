23b37d3f9ca5935236e9746f66bcc11a5ba719e1
ANAS727189 <anas23khan083@gmail.com>
2025-02-16 18:13:24 +0530

    feat: added more configurations and added cors for connecting backend with frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast';
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <BrowserRouter>
+  <Toaster
+      position="top-left"
+      reverseOrder={true}
+      toastOptions={{ duration: 5000 }}
+    />
     <App />
-  </StrictMode>,
+  </BrowserRouter>
 )
--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,17 @@
-import express from "express";
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
-
-
-
+app.use(express.json({limit: '16kb'}));
+app.use(cookieParser());
+const CorsOption = {
+    origin: process.env.CLIENT_URI,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+};
+app.use(cors(CorsOption));
+app.use(express.urlencoded({extended: true, limit: '16kb'}));
 
 
 export {app};
